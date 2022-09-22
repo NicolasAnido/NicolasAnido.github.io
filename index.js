0a1bcd7e3e7fe5f9a97361a94b01664d95c0228f
NicolasAnido <nicolas.anido@despegar.com>
2022-09-22 15:53:54 -0300

    Minor changes

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 async function readTag() {
+    console.log('Clicked readTag');
     if ("NDEFReader" in window) {
       const ndef = new NDEFReader();
       try {
         await ndef.scan();
-        ndef.onreading = event => {
-          const decoder = new TextDecoder();
-          for (const record of event.message.records) {
-            console.log("Record type:  " + record.recordType);
-            console.log("MIME type:    " + record.mediaType);
-            console.log("=== data ===\n" + decoder.decode(record.data));
-          }
-        }
+        console.log('Scan Started');
+        // ndef.onreading = event => {
+        //   const decoder = new TextDecoder();
+        //   for (const record of event.message.records) {
+        //     console.log("Record type:  " + record.recordType);
+        //     console.log("MIME type:    " + record.mediaType);
+        //     console.log("=== data ===\n" + decoder.decode(record.data));
+        //   }
+        // }
+        ndef.addEventListener("readingerror", () => {
+            console.log("Argh! Cannot read data from the NFC tag. Try another one?");
+          });
+      
+          ndef.addEventListener("reading", ({ message, serialNumber }) => {
+            console.log(`> Serial Number: ${serialNumber}`);
+            console.log(`> Records: (${message.records.length})`);
+          });
       } catch(error) {
         console.log(error);
       }
